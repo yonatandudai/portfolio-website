0296b0b8f26127305bb7ec51b60aa522f068777b
yonatandudai <duday@post.bgu.ac.il>
2024-11-02 01:47:06 +0200

    modified skills.js

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -6,46 +6,45 @@ function Skills() {
       <h2>Skills</h2>
       <div className="all_skills">
       <div className="skill">
-          <img src="../assets/java.png" target="_blank" alt="Java icon"/>
+          <img src="../assets/java.png" alt="Java icon"/>
           <h6>Java</h6>
         </div>
         <div className="skill">
-          <img src="../assets/python.png" target="_blank" alt="Python icon"/>
+          <img src="../assets/python.png" alt="Python icon"/>
           <h6>Python</h6>
         </div>
         <div className="skill">
-          <img src="../assets/html5.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/html5.png" alt="HTML icon"/>
           <h6>HTML</h6>
         </div>
         <div className="skill">
-          <img src="../assets/CSS3.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/CSS3.png" alt="CSS icon"/>
           <h6>CSS</h6>
         </div>
         <div className="skill">
-          <img src="../assets/js.jpeg" target="_blank" alt="HTML icon"/>
+          <img src="../assets/js.jpeg" alt="CSS icon"/>
           <h6>JavaScript</h6>
         </div>
         <div className="skill">
-          <img src="../assets/react.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/react.png" alt="React icon"/>
           <h6>React</h6>
         </div>
         <div className="skill">
-          <img src="../assets/node.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/node.png" alt="Node icon"/>
           <h6>Node.js</h6>
         </div>
         <div className="skill">
-          <img src="../assets/mongo.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/mongo.png" alt="MongoDB icon"/>
           <h6>MongoDB</h6>
         </div>
         <div className="skill">
-          <img src="../assets/c-logo.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/c-logo.png" alt="C icon"/>
           <h6>C</h6>
         </div>
         <div className="skill">
-          <img src="../assets/C-plus-logo.png" target="_blank" alt="HTML icon"/>
+          <img src="../assets/C-plus-logo.png" alt="HTML icon"/>
           <h6>C++</h6>
         </div>
-        {/* Repeat for other skills */}
       </div>
     </section>
   );
