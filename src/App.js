13fa6f1711b7e268603ac379678c5417ab805d3d
yonatandudai <duday@post.bgu.ac.il>
2024-11-01 19:05:32 +0200

    changed structure

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer'
-import './App.css';
+import '../src/styles/App.css';
 
 function App() {
   return (
